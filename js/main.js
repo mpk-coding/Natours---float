f713e61176dd5eb35e62002e2ef0a53ebd15406f
nadmiar-wszechswiata <thamak@gmail.com>
2020-03-19 09:04:25 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// REMOVE CSS TRANSITIONS = 0 AFTER PAGE LOAD
+window.onload = function onload() {
+    var x = document.getElementsByClassName('preload')[0];
+    x.classList.remove('preload');
+  }
